ff8e7103f5fcec75d29ee7d14d87c6312be4e7aa
Christina Howard <clhoward33@icloud.com>
2020-09-11 21:37:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(counter)
   if counter.empty?
     puts "The line is currently empty."
   else current_line = "The line is currently:"
-    counter.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
+    counter.each.with_index(1) do |person, a|
+      current_line << " #{a}. #{person}"
     end
     puts current_line
   end
