7de8c54a7f46f9e59078ef71ccf67b92bcbb9a9d
Christina Howard <clhoward33@icloud.com>
2020-09-11 20:59:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,25 @@
 def deli_counter
-katz_deli = []
-line(katz_deli, "The line is currently empty.")
+line(katz_deli)
+
   katz_deli = []
-take_a_number(katz_deli, "Ada")
+
+  take_a_number(katz_deli, "Ada")
+  take_a_number(katz_deli, "Grace")
+  take_a_number(katz_deli, "Kent")
+
+line(katz_deli)
+
+now_serving(katz_deli)
+
+line(katz_deli)
+
+take_a_number(katz_deli, "Matz")
+
+line(katz_deli)
+
+now_serving(katz_deli)
+
+line(katz_deli)
 
 
 
