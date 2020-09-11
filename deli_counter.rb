ca59dfdac1878152a1953e8d1e4dc51399fd889c
Christina Howard <clhoward33@icloud.com>
2020-09-11 22:06:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,7 @@
 def line(counter)
   if counter.empty?
     puts "The line is currently empty."
-  else
-    current_line = "The line is currently:"
+  else current_line = "The line is currently:"
     counter.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
     end
@@ -13,4 +12,13 @@ end
 def take_a_number(counter, name)
   counter << name
   puts "Welcome, #{name}. You are number #{counter.length} in line."
+end
+
+def now_serving(counter)
+  if counter.empty?
+    puts "There is nobody waiting to be served."
+  else 
+    puts "Currently serving #{counter.first}."
+    counter.shift
+  end
 end
