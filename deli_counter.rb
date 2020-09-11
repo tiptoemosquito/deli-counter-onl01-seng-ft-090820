762340ec1dc85b059a8380c9e5c7786afb8dcb62
Christina Howard <clhoward33@icloud.com>
2020-09-11 21:48:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,12 +9,7 @@ def line(counter)
   end
 end
 
-def line(counter)
-  if counter.empty?
-    puts "Welcome, #{person}. You are number one in line."
-  else current_line = "The line is currently:"
-    count.each.with_index(1) do |person, a|
-      current_line.push "#{a}. #{person}"
-    end
-    puts current_line
-  end
+def line(counter, name)
+  deli.push name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
